8883067105a6f58ad352766b4780b97045d4fa5e
TBK123 <lol383tarek@hotmail.com>
2023-09-05 21:14:47 -0500

    Re changed everything to test from the beginning

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,56 +1,70 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[ show edit update destroy ]
 
-    # Index action to display all books
-    def index
-      @books = Book.all
-    end
-  
-    # New action for creating a new book
-    def new
-      @book = Book.new
-    end
-  
-    # Create action to save a new book to the database
-    def create
-      @book = Book.new(book_params)
+  # GET /books or /books.json
+  def index
+    @books = Book.all
+  end
+
+  # GET /books/1 or /books/1.json
+  def show
+  end
+
+  # GET /books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books or /books.json
+  def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
       if @book.save
-        redirect_to books_path, notice: 'Book was successfully created.'
+        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
+        format.json { render :show, status: :created, location: @book }
       else
-        render :new
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
-    end
-  
-    # Edit action to edit an existing book
-    def edit
-      @book = Book.find(params[:id])
-    end
-  
-    # Update action to update an existing book in the database
-    def update
-      @book = Book.find(params[:id])
-      if @book.update(book_params)
-        redirect_to book_path(@book), notice: 'Book was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    # Show action to display details of a specific book
-    def show
-      @book = Book.find(params[:id])
-    end
-  
-    # Destroy action to delete a book from the database
-    def destroy
-      @book = Book.find(params[:id])
-      @book.destroy
-      redirect_to books_path, notice: 'Book was successfully deleted.'
-    end
-  
-    private
-  
-    # Strong parameters to allow only specific attributes
-    def book_params
-      params.require(:book).permit(:title, :author, :price, :published_date)
     end
   end
+
+  # PATCH/PUT /books/1 or /books/1.json
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /books/1 or /books/1.json
+  def destroy
+    @book.destroy
+
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:title)
+    end
+end
